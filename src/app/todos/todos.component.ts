48ea7089b13de7d5002681c318c7763692105bb0
jinalshah999 <jinalshah999@gmail.com>
2020-09-14 09:12:17 +0530

    done

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -12,8 +12,20 @@ export class TodosComponent implements OnInit {
   arrTodos: Todo[] = [
     new Todo(1, 'wear a mask if you go out', 'done'),
     new Todo(2, 'email to your mananger', 'pending'),
+    new Todo(3, 'demo tasks', 'pending'),
+    new Todo(4, 'some description', 'done'),
   ];
   constructor() {}
 
   ngOnInit(): void {}
+  onDeleteTask(item: Todo) {
+    this.arrTodos.splice(this.arrTodos.indexOf(item), 1);
+  }
+  onEditTask(item: Todo) {
+    if (item.status == 'done') {
+      item.status = 'pending';
+    } else {
+      item.status = 'done';
+    }
+  }
 }
