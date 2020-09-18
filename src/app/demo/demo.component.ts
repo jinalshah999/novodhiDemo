355c37c4ea1471479122f06fe8417684206aa00b
jinalshah999 <jinalshah999@gmail.com>
2020-09-18 09:31:21 +0530

    pipes

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -9,9 +9,19 @@ export class DemoComponent implements OnInit {
   a: number = 0;
   b: number = 0;
   sum: number = 0;
+  ans: number = 0;
   no1: number = 10;
   str: string = 'Jinal Shah';
+  str1: string = 'asfasfasd';
   flag: boolean = false;
+  name: string = 'JINAL SHAH';
+  price: number = 123;
+  today: number = Date.now();
+  object: Object = {
+    foo: 'bar',
+    baz: 'qux',
+    nested: { xyz: 3, numbers: [1, 2, 3, 4, 5] },
+  };
   constructor() {}
 
   ngOnInit(): void {}
@@ -24,5 +34,8 @@ export class DemoComponent implements OnInit {
 
   onAddClick() {
     this.sum = this.a + this.b;
+  }
+  onAdd1Click(no1, no2) {
+    this.ans = parseInt(no1) + parseInt(no2);
   }
 }
