26ae5ec1d1119cffdff9c17754c6d5e6d7006b9f
jinalshah999 <jinalshah999@gmail.com>
2020-09-23 09:27:20 +0530

    form pushed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ParentComponent } from './parent/parent.component';
 import { ChildComponent } from './child/child.component';
 import { HeaderComponent } from './header/header.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
     ChildComponent,
     HeaderComponent,
     PagenotfoundComponent,
+    EmployeeComponent,
   ],
   imports: [BrowserModule, FormsModule, arrRouting],
   providers: [],
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,12 +5,13 @@ import { TodosComponent } from './todos/todos.component';
 import { ForDemosComponent } from './for-demos/for-demos.component';
 import { ParentComponent } from './parent/parent.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
-
+import { EmployeeComponent } from './employee/employee.component';
 const arr: Routes = [
   { path: '', component: DemoComponent },
   { path: 'todo', component: TodosComponent },
   { path: 'fordemo', component: ForDemosComponent },
   { path: 'product', component: ParentComponent },
+  { path: 'empadd', component: EmployeeComponent },
   { path: 'pagenotfound', component: PagenotfoundComponent },
   { path: '**', redirectTo: '/pagenotfound' },
 ];
