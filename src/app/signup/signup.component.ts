3d350dfab5742754c4f5b830feb53c22bf0c79ba
jinalshah999 <jinalshah999@gmail.com>
2020-09-28 10:01:40 +0530

    custom validators

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -27,17 +27,31 @@ export class SignupComponent implements OnInit {
         Validators.required,
         Validators.email,
       ]),
+      password_group: new FormGroup(
+        {
+          password: new FormControl(null, [Validators.required]),
+          confirm_password: new FormControl(null, [Validators.required]),
+        },
+        [this.matchPasswords.bind(this)]
+      ),
     });
   }
   onSignup() {
     console.log(this.signupForm);
   }
-
   invalidUserNameValidation(
     control: AbstractControl
   ): { [key: string]: boolean } {
     if (this.invalidNamesArr.indexOf(control.value) >= 0) {
       return { invalidName: true };
+    }
+    return null;
+  }
+  matchPasswords(control: AbstractControl): { [key: string]: boolean } {
+    if (
+      control.get('password').value != control.get('confirm_password').value
+    ) {
+      return { passwordMatchError: true };
     }
     return null;
   }
